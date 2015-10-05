d8fe635a8ae9142332c90376a89cfa4fb5ed8691
jonst <thirj525@newschool.edu>
2015-10-05 13:51:11 +0000

    updating assignment 4 and database

--- wassign04.js
+++ wassign04.js
@@ -1,31 +1,22 @@
-// IN THE MONGO SHELL: 
-//   CREATE DATABASE citibike AND SWITCH TO IT WITH: 
-//      use citibike
-//   CREATE COLLECTION stations WITH: 
-//      db.createCollection('stations')
 
-var request = require('request');
 var fs = require('fs');
-
+//get and parse the JSON file from the assignment 3
 var fileContent = fs.readFileSync('/home/ubuntu/workspace/data/aameetingsMapArray.txt');
 var meetingsData = JSON.parse(fileContent);
 
-//    Connection URL
+//set up the url to the database
     var url = 'mongodb://' + process.env.IP + ':27017/aameetings';
 
-    // Retrieve
+    //require Mongo!!
     var MongoClient = require('mongodb').MongoClient; // npm install mongodb
 
     MongoClient.connect(url, function(err, db) {
         if (err) {return console.dir(err);}
-
+//I created a document called "meetings" inside the "aameetings" database
         var collection = db.collection('meetings');
 
-        // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+        // put the meetings data we have into the database
         collection.insert(meetingsData);
-  //      for (var i=0; i < stationData.stationBeanList.length; i++) {
-  //          collection.insert(stationData.stationBeanList[i]);
-  //          }
-        db.close();
+         db.close();
 
-    }); //MongoClient.connect
+    });
